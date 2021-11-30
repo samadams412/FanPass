38d0227c706f9b946bcc295a705a5f5d2796c771
Samuel Adams <samueladams412@gmail.com>
2021-11-29 20:02:36 -0600

    added compression npm package to midify files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const compression = require('compression');
 const routes = require('./controllers/');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
@@ -11,7 +12,7 @@ const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.use(compression());
 const sess = {
   secret: 'Super secret secret',
   cookie: {
